b9f0284647c7bc16292e21d23998ed63d3b0e671
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-11-19 00:02:22 +0100

    modified class_autoloader.php; added database model

--- include/class_autoloader.php
+++ include/class_autoloader.php
@@ -3,8 +3,19 @@
 spl_autoload_register('class_autoloader');
 
 function class_autoloader($class_name) {
-  $file_name = strtolower($class_name);
-  $path = "../src/$file_name.php";
+  if (substr($class_name, -5) === 'Model') {
+    $parent_folder = 'models';
+  } elseif (substr($class_name, -4) === 'View') {
+    $parent_folder = 'views';
+  } elseif (substr($class_name, -10) === 'Controller') {
+    $parent_folder = 'controllers';
+  }
+
+  if ($parent_folder === '') {
+    $path = "../src/$class_name.php";
+  } else {
+    $path = "../src/$parent_folder/$class_name.php";
+  }
 
   if (!file_exists($path)) {
     throw new \Exception('Loaded class not found');
---
+++ src/models/DatabaseModel.php
@@ -0,0 +1,43 @@
+<?php
+require('../tools/sql.php');
+
+abstract class DatabaseModel
+{
+
+  private $pdo;
+
+  protected function connection()
+  {
+    $this->pdo = new PDO(
+      'mysql:host=' . DB_HOST. ';port=' . DB_PORT. ';charset=' . DB_CHAR . ';dbname=' . DB_NAME,
+      DB_USER,
+      DB_PASS,
+      PDO_OPTIONS
+    );
+
+    if (empty($this->pdo))
+    {
+      throw new \Exception('Connection failed.');
+      return false;
+    }
+
+    echo 'Connected to ' . DB_NAME . '.<br>'; // for debugging
+    return true;
+  }
+
+  protected function run_query($sql, $placeholders = [])
+  {
+    if ($this->connection() === true)
+    {
+      if (empty($placeholders))
+      {
+        return $this->pdo->query($sql)->fetchAll();
+      }
+      else
+      {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($placeholders);
+      }
+    }
+  }
+}
