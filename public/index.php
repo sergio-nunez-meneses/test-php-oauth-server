321e0ad57709f0f0164bf83fcf74de80b38054d0
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-11-24 09:26:25 +0100

    minor change in index.php

--- public/index.php
+++ public/index.php
@@ -4,11 +4,9 @@ require_once('../include/class_autoloader.php');
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-// if ($uri[1] === '') {
-//   UserController::login_form();
-// }
-
-if ($uri[1] === 'token') {
+if ($uri[1] === '') {
+  UserController::login_form();
+} elseif ($uri[1] === 'token') {
   CurlController::response_test();
 } elseif ($uri[1] === 'redirected') {
   CurlController::redirection_test();
