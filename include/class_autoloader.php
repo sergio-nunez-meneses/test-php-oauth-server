fc266692402025e90ae8d4218a57ecaaf5447597
sergio-nunez-meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-12-10 15:41:32 +0100

    deleted views and templates folders; updated index controller and class_autoloader.php file

--- include/class_autoloader.php
+++ include/class_autoloader.php
@@ -5,8 +5,6 @@ spl_autoload_register('class_autoloader');
 function class_autoloader($class_name) {
   if (substr($class_name, -5) === 'Model') {
     $parent_folder = 'models';
-  } elseif (substr($class_name, -4) === 'View') {
-    $parent_folder = 'views';
   } elseif (substr($class_name, -10) === 'Controller') {
     $parent_folder = 'controllers';
   }
--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -5,17 +5,14 @@ class IndexController
 
   public static function route_requests($uri, $request_method)
   {
-    $path = $uri[1];
-    $endpoints = isset($uri[2]) ? $uri[2] : '';
-    $valid_uris = ['', 'auth', 'request_token', 'verify_token', 'access_token', 'refresh_token', 'revoke_token'];
+    $valid_uris = ['auth', 'request_token', 'verify_token', 'access_token', 'refresh_token', 'revoke_token'];
 
-    if (in_array($path, $valid_uris) && in_array($endpoints, $valid_uris))
+    if (in_array($uri[1], $valid_uris) && in_array($uri[2], $valid_uris))
     {
-      if ($path === '' && $request_method === 'GET')
-      {
-        UserController::login_form();
-      }
-      elseif ($path === 'auth')
+      $path = $uri[1];
+      $endpoints = $uri[2];
+
+      if ($path === 'auth')
       {
         if ($endpoints === 'request_token' && $request_method === 'POST')
         {
@@ -44,7 +41,7 @@ class IndexController
     }
     else
     {
-      throw new \Exception('Page ' . strtoupper($uri) . ': Not found.');
+      throw new \Exception('Page ' . strtoupper($uri[1]) . ': Not found.');
     }
   }
 }
