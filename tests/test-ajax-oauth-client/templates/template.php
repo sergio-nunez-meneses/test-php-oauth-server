a0ea643735b9f829607476af3a3b124221833470
sergio-nunez-meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-01-14 17:00:41 +0100

    ajax client application: styled html elements

--- tests/test-ajax-oauth-client/templates/template.php
+++ tests/test-ajax-oauth-client/templates/template.php
@@ -15,38 +15,42 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Sergio NUNEZ MENESES">
     <link rel="stylesheet" href="public/css/style.css">
-    <title>OAuth Server Test</title>
+    <title>OAuth Client Application</title>
   </head>
   <body>
 
-    <header id="mainHeader">
-      <nav id="headerLinks">
-        <a class="navLink" href="/">
-          <img src="public/img/logo_davi.png" alt="logo">
+    <header class="header-container">
+      <nav class="header-items">
+        <a class="nav-item" href="/">
+          <img id="logo" src="public/img/logo_davi.png" alt="logo">
         </a>
-        <a class="navLink" href="">A propos</a>
-        <a class="navLink" href="">Contact</a>
+        <a class="nav-item" href="#">A propos</a>
+        <a class="nav-item" href="#">Contact</a>
       </nav>
     </header>
 
-    <div id=column>
-      <main id="content">
+    <div class="columns-container">
+      <main class="left-column">
         <h2>Retorik The Emotional AI Platform</h2>
         <p>Pour créer des experts digitaux et ainsi être plus proche des utilisateurs, Davi a développé une IA adaptée aux besoins des entreprises qui réalisent des tâches complexes au service des humains.</p>
 
-        <input id="username" class="login-input" type="text" name="username" value="username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'username';}">
-        <input class="login-input" type="password" name="password" value="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}">
+        <h2>S'IDENTIFIER</h2>
+        <input id="username" type="text" name="username" value="username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'username';}">
+        <input type="password" name="password" value="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}">
+        <button type="button" name="request" value="POST">
+          ACCÉDER
+        </button>
       </main>
 
-      <aside id="sidebar">
+      <aside class="sidebar">
         <h2>NOTRE MÉTIER</h2>
         <p>DAVI est un éditeur de logiciels en mode SaaS qui dispose des expertises dans les domaines de l’IA, de l’Affective Computing et des IHM.</p>
         <P>Pour mener à bien ses missions, DAVI dispose de compétences en ingénierie cognitive, en développement d’applications logicielles et en développement 3D.</p>
       </aside>
     </div>
 
-    <footer id="mainFooter">
-      <p class="footer-text">Copyright © <a class="footer-text" href="https://davi.ai">davi</a> 2015 - 2020</p>
+    <footer class="footer-container">
+      <p class="footer-text">Copyright © <a class="footer-text" href="https://davi.ai">DAVI</a> The Humanizers 2020</p>
     </footer>
 
     <!-- <script src="public/js/script.js"></script> -->
